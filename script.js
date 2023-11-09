57234588698b8105bc151613b632ba0098c29320
kaleian <kaleian54@gmail.com>
2023-11-09 22:23:07 +0000

    Added functionality the service section

---
+++ script.js
@@ -0,0 +1,7 @@
+var service = document.getElementById("service")
+var dropdown = document.getElementById("dropdown")
+
+service.addEventListener( "click", ()=>{
+    
+    dropdown.style.display = "block"
+})
